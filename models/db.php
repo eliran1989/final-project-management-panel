941873fa44ae29693ca6ce13b6ca51d379907331
Eliran Abuhatzira <elirana1989@gmail.com>
2016-11-11 16:36:57 +0000

    db need to work in localhost and server

--- models/db.php
+++ models/db.php
@@ -1,8 +1,17 @@
 <?php
-
+	
+	if ($_SERVER['HTTP_HOST']=="127.0.0.1"){
+	define('DB_HOST',"localhost");
+	define('DB_NAME', "spacegym");
+	define('DB_USER', "root");
+	define('DB_PASS', ""); 
+	}
+	else{
 	define('DB_HOST',"mysql.hostinger.co.il");
 	define('DB_NAME', "u893376912_space");
 	define('DB_USER', "u893376912_elira");
 	define('DB_PASS', "201643202"); 
+	}
+
 
 ?>
