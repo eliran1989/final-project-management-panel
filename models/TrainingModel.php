3848979001899231dbc90190e5362123433110da
Eliran <elirana1989@gmail.com>
2017-08-30 00:06:23 +0300

    no message

--- models/TrainingModel.php
+++ models/TrainingModel.php
@@ -139,7 +139,7 @@ public function __construct(){
               if($totalTrainings==0){
                 return 0;
               }else{
-                echo floor($totalTrainings/$trainingsSum*100);
+                echo intval($totalTrainings/$trainingsSum*100);
               }
 
 
